d127b9ca0b44e78380915defcc4a08b9db789323
Lucas Olmedo Silva <lsilva@meeta.com.br>
2020-04-29 17:45:35 -0300

    estructure settings page

--- src/app/settings-page/settings-page.page.ts
+++ src/app/settings-page/settings-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings-page',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPagePage implements OnInit {
 
-  constructor() { }
+  selectedLanguage: string = 'Português';
+
+  constructor(public alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+  async changeLanguage() {
+    const alert = await this.alertCtrl.create({
+      inputs: [
+        {
+          name: 'radio1',
+          type: 'radio',
+          label: 'Português',
+          value: 'Português',
+          checked: this.selectedLanguage === 'Português',
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: 'English',
+          value: 'English',
+          checked: this.selectedLanguage === 'English',
+        },
+        {
+          name: 'radio3',
+          type: 'radio',
+          label: 'Español',
+          value: 'Español',
+          checked: this.selectedLanguage === 'Español',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            this.selectedLanguage = this.selectedLanguage;
+          }
+        },
+        {
+          text: 'Ok',
+          handler: data => {
+            this.selectedLanguage = data;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
